d7cd7f80f9559b66819e4ca4849ff257c56df9a5
AungKoLin828 <aungko.linn404@gmail.com>
2024-08-06 16:38:18 +0630

    Update Src

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Footer.css'; // Create a CSS file for styling the footer
 
 const Footer = () => {
+  let date = new Date();
+  let year = date.getFullYear();
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; Design & Developed By AungKoLin. 2024 Doctors & Patients Appointments System. All Rights Reserved.</p>
+        <p>&copy; Design & Developed By AungKoLin. {year} Doctors & Patients Appointments System. All Rights Reserved.</p>
       </div>
     </footer>
   );
