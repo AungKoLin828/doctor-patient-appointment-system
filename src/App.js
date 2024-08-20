a0033ff0caa2274a6b9b01fa09811dbd81f9181c
AungKoLin828 <aungko.linn404@gmail.com>
2024-08-21 00:05:52 +0630

    Update Src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes,Link  } from 'react-router-dom';
-import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -14,33 +14,6 @@ import DoctorProfile from './components/User/DoctorProfile';
 import { AuthProvider } from './components/AuthContext';
 
 function App() {
-
-  // Check inactivity on page load
-  useEffect(() => {
-    const checkInactivity = () => {
-      const lastActivity = localStorage.getItem('lastActivity');
-      const now = new Date().getTime();
-      const thirtyMinutes = 30 * 60 * 1000;
-
-      if (lastActivity && (now - lastActivity) > thirtyMinutes) {
-        localStorage.clear();
-        alert('You have been logged out due to inactivity.');
-        Link('/login'); // Redirect to login
-      }
-    };
-
-    const resetTimer = () => {
-      localStorage.setItem('lastActivity', new Date().getTime());
-    };
-
-    // Attach event listeners to reset the timer on activity
-    window.onload = resetTimer;
-    window.onmousemove = resetTimer;
-    window.onkeypress = resetTimer;
-
-    // Call the inactivity check function on page load
-    checkInactivity();
-  }, []);
 
   return (
     <div className="App">
